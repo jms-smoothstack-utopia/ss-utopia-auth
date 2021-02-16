4c17632cc0df1b8b5ef6ea6b9abf1841f08b4612
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-02-16 17:18:08 -0500

    Initial commit

---
+++ src/main/java/com/ss/utopia/auth/UtopiaAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ss.utopia.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtopiaAuthServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UtopiaAuthServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ss/utopia/auth/UtopiaAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.utopia.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtopiaAuthServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
