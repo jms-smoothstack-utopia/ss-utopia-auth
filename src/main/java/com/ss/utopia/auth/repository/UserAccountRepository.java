2bf8657114fb370ba0e5206146a6afd398d1529e
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-02-18 14:29:46 -0500

    Fix repo ID class

--- src/main/java/com/ss/utopia/auth/repository/UserAccountRepository.java
+++ src/main/java/com/ss/utopia/auth/repository/UserAccountRepository.java
@@ -2,11 +2,12 @@ package com.ss.utopia.auth.repository;
 
 import com.ss.utopia.auth.entity.UserAccount;
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, UUID> {
 
   Optional<UserAccount> findByEmail(String email);
 }
